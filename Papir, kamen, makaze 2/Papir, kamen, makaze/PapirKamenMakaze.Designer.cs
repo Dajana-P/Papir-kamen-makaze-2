4c7843c96b7e14b2aa1bf31c6586cce514760eae
Dajana-P <142403457+Dajana-P@users.noreply.github.com>
2023-10-22 22:28:46 +0200

    Add files via upload

---
+++ Papir, kamen, makaze 2/Papir, kamen, makaze/PapirKamenMakaze.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Papir__kamen__makaze
+{
+    partial class PapirKamenMakaze
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pBLeft = new System.Windows.Forms.PictureBox();
+            this.pBRight = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.timerRound = new System.Windows.Forms.Timer(this.components);
+            this.lblRoundNumber = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pBLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBRight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pBLeft
+            // 
+            this.pBLeft.Image = global::Papir__kamen__makaze.Properties.Resources.img;
+            this.pBLeft.Location = new System.Drawing.Point(62, 67);
+            this.pBLeft.Name = "pBLeft";
+            this.pBLeft.Size = new System.Drawing.Size(211, 282);
+            this.pBLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBLeft.TabIndex = 2;
+            this.pBLeft.TabStop = false;
+            // 
+            // pBRight
+            // 
+            this.pBRight.Image = global::Papir__kamen__makaze.Properties.Resources.img;
+            this.pBRight.Location = new System.Drawing.Point(522, 67);
+            this.pBRight.Name = "pBRight";
+            this.pBRight.Size = new System.Drawing.Size(209, 282);
+            this.pBRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBRight.TabIndex = 1;
+            this.pBRight.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(341, 169);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(113, 71);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.Location = new System.Drawing.Point(341, 364);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(33, 13);
+            this.lblWinner.TabIndex = 4;
+            this.lblWinner.Text = "Label";
+            // 
+            // timerRound
+            // 
+            this.timerRound.Interval = 3000;
+            this.timerRound.Tick += new System.EventHandler(this.timerRound_Tick);
+            // 
+            // lblRoundNumber
+            // 
+            this.lblRoundNumber.AutoSize = true;
+            this.lblRoundNumber.Location = new System.Drawing.Point(12, 9);
+            this.lblRoundNumber.Name = "lblRoundNumber";
+            this.lblRoundNumber.Size = new System.Drawing.Size(42, 13);
+            this.lblRoundNumber.TabIndex = 5;
+            this.lblRoundNumber.Text = "Round ";
+            // 
+            // PapirKamenMakaze
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblRoundNumber);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pBLeft);
+            this.Controls.Add(this.pBRight);
+            this.Name = "PapirKamenMakaze";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Papir, kamen, makaze";
+            ((System.ComponentModel.ISupportInitialize)(this.pBLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBRight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pBRight;
+        private System.Windows.Forms.PictureBox pBLeft;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Timer timerRound;
+        private System.Windows.Forms.Label lblRoundNumber;
+    }
+}
---
+++ Papir, kamen, makaze 2/Papir, kamen, makaze/PapirKamenMakaze.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Papir__kamen__makaze
+{
+
+    public partial class PapirKamenMakaze : Form
+    {
+        int roundNum = 0;
+        Random rand = new Random();
+        StreamWriter sw = new StreamWriter(@"C:\Users\PD\Desktop\Rezultati\Rezultat.txt");
+        List<string> Log = new List<string>();
+        int brKamen, brPapir, brMakaze, brNereseno;
+
+        public PapirKamenMakaze()
+        {
+            InitializeComponent();
+            lblWinner.Text = "";
+            lblRoundNumber.Text = "";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (btnStart.Text == "Start")
+            {
+                brKamen = 0;
+                brPapir = 0;
+                brMakaze = 0;
+                brNereseno = 0;
+                Round();
+                timerRound.Start();
+                btnStart.Text = "Stop";
+            }
+            else
+            {
+                timerRound.Dispose();
+                Results();
+                btnStart.Text = "Start";
+                pBLeft.Image = Properties.Resources.img;
+                pBRight.Image = Properties.Resources.img;
+                lblWinner.Text = "";
+                lblRoundNumber.Text = "";
+                roundNum = 0;
+            }
+
+        }
+
+        private Image GetImage(int br)
+        {
+            Image pic;
+            switch (br)
+            {
+                case 1:
+                    pic = Properties.Resources.p; //papir = 1
+                    break;
+                case 2:
+                    pic = Properties.Resources.r; //kamen = 2
+                    break;
+                case 3:
+                    pic = Properties.Resources.s; //makaze = 3
+                    break;
+                default:
+                    pic = Properties.Resources.img;
+                    break;
+            }
+            return pic;
+        }
+        private int RandomNumber()
+        {
+            return rand.Next(1, 4);
+        }
+
+        private String Winner(int brL, int brR)
+        {
+            if ((brL == 1 && brR == 2) || (brL == 2 && brR == 1))
+            {
+                ++brPapir;
+                return "Papir je pobedio";
+            }
+            else if ((brL == 1 && brR == 3) || (brL == 3 && brR == 1))
+            {
+                ++brMakaze;
+                return "Makaze su pobedile";
+            }
+            else if ((brL == 2 && brR == 3) || (brL == 3 && brR == 2))
+            {
+                ++brKamen;
+                return "Kamen je pobedio";
+            }
+            else
+            {
+                ++brNereseno;
+                return "Nereseno";
+            }
+        }
+
+        private void timerRound_Tick(object sender, EventArgs e)
+        {
+            Round();
+        }
+
+        private void Round()
+        {
+            int rbrL = RandomNumber();
+            int rbrR = RandomNumber();
+            pBLeft.Image = GetImage(rbrL);
+            pBRight.Image = GetImage(rbrR);
+            lblWinner.Text = Winner(rbrL, rbrR);
+            lblRoundNumber.Text = "Round: " + ++roundNum;
+            Log.Add(lblRoundNumber.Text + "\t" + lblWinner.Text);
+        }
+
+        private void Results()
+        {
+            foreach (string rezultat in Log)
+            {
+                sw.WriteLine(rezultat);
+            }
+            sw.WriteLine();
+            sw.WriteLine("Broj pobeda kamena: " + brKamen);
+            sw.WriteLine("Broj pobeda papira: " + brPapir);
+            sw.WriteLine("Broj pobeda makaza: " + brMakaze);
+            sw.WriteLine("Broj neresenih rundi: " + brNereseno);
+            sw.Close();
+        }
+    }
+}
